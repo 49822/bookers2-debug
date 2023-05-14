7f29da109ff88fdd48638993836cb39165989f61
ShoheiyoShida <129791157+49822@users.noreply.github.com>
2023-05-14 14:20:35 +0000

    [Start] 1-3

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,15 +1,24 @@
-class FavoritesController < ApplicationController
+class RelationshipsController < ApplicationController
+  before_action :authenticate_user!
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
-    favorite.save
-    redirect_to request.referer
+    user = User.find(params[:user_id])
+    current_user.follow(user)
+		redirect_to request.referer
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
-    favorite.destroy
-    redirect_to request.referer
+    user = User.find(params[:user_id])
+    current_user.unfollow(user)
+		redirect_to request.referer
+  end
+
+  def followings
+    user = User.find(params[:user_id])
+		@users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+		@users = user.followers
   end
 end
